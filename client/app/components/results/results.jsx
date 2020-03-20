27e98f5d05317ce775e2a437d78f18e85e5e5ad2
Simao Nziaka <sdnziaka@gmail.com>
2020-03-20 16:39:32 +0100

    results more section

--- client/app/components/results/results.jsx
+++ client/app/components/results/results.jsx
@@ -1,5 +1,9 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
+// vendor
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
 // app
 import './results.css';
 import { Pageheader } from '../';
@@ -18,6 +22,8 @@ export function Results() {
     { name: 'Coffee Shop', icon: '' },
     { name: 'Coffee Shop', icon: '' },
     { name: 'Coffee Shop', icon: '' },
+    { name: 'Coffee Shop', icon: '' },
+    { name: 'Coffee Shop', icon: '' },
     { name: 'Coffee Shop', icon: '' }
   ]);
 
@@ -31,8 +37,25 @@ export function Results() {
         ]}
       />
       <section className="main-section">
-        <div id="user-results" className="main-box">
-          {results.map((res, i) => <Card key={i} name={res.name} icon={res.icon} />)}
+        <div id="results-wrap" className="main-box">
+          <div id="user-results">
+            {results.map((res, i) => <Card key={i} name={res.name} icon={res.icon} />)}
+          </div>
+          <div id="results-more">
+            <Button
+              id="go-back-btn"
+              color="primary"
+            >
+              <Link to="/">Go Back</Link>
+            </Button>
+            <Button
+              id="gen-more-btn"
+              variant="contained"
+              color="primary"
+            >
+              Generate More
+            </Button>
+          </div>
         </div>
       </section>
     </div>
