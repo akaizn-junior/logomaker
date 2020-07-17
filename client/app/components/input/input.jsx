5dbd39c8edbebcdf621ce20c5ec2e44522893ee5
Simao Nziaka <sdnziaka@gmail.com>
2020-07-17 07:11:25 +0100

    session storage remember input data

--- client/app/components/input/input.jsx
+++ client/app/components/input/input.jsx
@@ -1,12 +1,30 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './input.css';
 
 export function Input(props) {
-  const { label, className, ...rest } = props;
+  const {
+    label,
+    defaultValue,
+    className,
+    remember,
+    onBlur,
+    ...rest
+  } = props;
+
+  const _remember = remember && typeof remember === 'string' ? remember : false;
+  const dValue = defaultValue || '';
+  const remembered = sessionStorage.getItem(_remember);
+
   let inputClassName = 'app-input__textfield';
   className && (inputClassName = `${inputClassName} ${className}`);
+
+  useEffect(() => {
+    if (remembered) {
+      onBlur({ target: { value: remembered } });
+    }
+  }, [remembered]);
 
   return (
     <div
@@ -21,6 +39,15 @@ export function Input(props) {
       </label>}
       <input
         className={inputClassName}
+        defaultValue={
+          remembered && !dValue
+            ? remembered
+            : dValue
+        }
+        onBlur={e => {
+          _remember && sessionStorage.setItem(_remember, e.target.value);
+          onBlur(e);
+        }}
         {...rest}
       />
     </div>
